b5872a80e2d778031cc94a8a431b5c5dfda72ad6
pfreifer <paul.freifer@hotmail.com>
2019-10-16 15:33:51 +0200

    construction function for the probabilist model based on grammars :rocket:

--- probabilistic_models/test_proba.py
+++ probabilistic_models/test_proba.py
@@ -1,3 +1,24 @@
 import grammars
+import construct
+
+fl = {
+    "1":"aaaaa123bb,ccc**azerty".split(","),
+    "2":"123jaaj!,888abc320,320lll123".split(",")
+}
+
+def build_ranked_dict(ordered_list):
+    return {word: idx for idx, word in enumerate(ordered_list, 1)}
+
+RD = {}
+
+print("aaa" in RD)
+
+def add_frequency_lists(frequency_lists_):
+    for name, lst in frequency_lists_.items():
+        RD[name] = build_ranked_dict(lst)
+
+add_frequency_lists(fl)
 
 print(grammars.char_type('m'))
+
+print(construct.construct_grammar_model())
