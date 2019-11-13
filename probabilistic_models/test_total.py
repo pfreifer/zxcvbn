b4654f52b05383fc300acb35edbafb9d02a7adf4
xavierefremont <xaviere.fremont@yahoo.fr>
2019-11-13 10:28:13 +0100

    test_proba OK

---
+++ probabilistic_models/test_total.py
@@ -0,0 +1,17 @@
+import zxcvbn.zxcvbn as zx
+
+results = zx.zxcvbn('immeuble')
+
+for x in results:
+    try:
+        k = x + " : "
+        for y in results[x]:
+            d = results[x][y]
+            if k != "":
+                print("\n", k)
+                k = ""
+            print("\t", y, ":", d)
+    except:
+        print("\n", x, ":", results[x])
+
+
