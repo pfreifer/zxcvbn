96abc455a2ff376b27641c02124f2a66cd41dc23
pfreifer <paul.freifer@hotmail.com>
2019-11-27 11:29:02 +0100

    construct dictionary

---
+++ src/probabilistic_models/construct_test.py
@@ -0,0 +1,23 @@
+import pickle
+import src.probabilistic_models.grammars as gr
+
+
+def construct_dict():
+    f = open("rockyou-withcount-utf-8.txt", "r")
+    i = 0
+    d = dict()
+    for l in f:
+        i+=1
+        n = int(l[0:8])
+        w = l[8:-1]
+        d[w] = n
+        if i%1000 == 0:
+            print(i)
+            print((w,n))
+    pickle.dump(d, open("rockyou_dictionary.p", "wb"))
+    print(d)
+
+
+(cb_counter, Q) = pickle.load(open("cb_dictionary.p", "rb"))
+(sb_counter, B) = pickle.load(open("sb_dictionary.p", "rb"))
+(l1,l2) = pickle.load(open("lists.p", "rb"))
